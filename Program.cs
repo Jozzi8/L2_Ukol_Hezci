05e4b4caa4df1e28fbcf9d966bfc3c45e77de3b0
ivako <ivako@DESKTOP-K1ID8ST>
2023-03-31 22:20:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿//S double, aby šlo vložit desetinné číslo a interpolac string
+
+using System;
+
+namespace L2_Ukol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte první číslo:");
+            string numOne = Console.ReadLine();
+            double numOneDou = double.Parse(numOne);
+
+            Console.WriteLine("Zadejte druhé číslo:");
+            string numTwo = Console.ReadLine();
+            double numTwoDou = double.Parse(numTwo);
+
+            Console.WriteLine("Vlož matematickou operaci (+, -, *, /):");
+            string oper = Console.ReadLine();
+
+            double vysledek;
+
+            if (oper == "+")
+            {
+                vysledek = numOneDou + numTwoDou;
+            }
+
+            else if (oper == "-")
+            {
+                vysledek = numOneDou - numTwoDou;
+            }
+
+            else if (oper == "*")
+            {
+                vysledek = numOneDou + numTwoDou;
+            }
+
+            else if (oper == "/")
+            {
+                if (numTwoDou == 0)
+                {
+                    Console.WriteLine("Nulou nelze dělit");
+                }
+                else
+                {
+                    vysledek = numOneDou / numTwoDou;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Povolené operace jsou pouze +, -, *, /");
+            }
+
+            Console.WriteLine("Výsledek: "+numOneDou+" "+oper+" "+numTwoDou+" = "+vysledek);
+            //Console.WriteLine($"Výsledek: {numOneDou} {oper} {numTwoDou} = {vysledek}");
+        }
+    }
+}
